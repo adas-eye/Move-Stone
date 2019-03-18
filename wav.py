9b27d75f8545d017c4b1bf272f9469980e7e7f4b
LeslieZhao <2289970030@qq.com>
2019-03-18 19:47:35 +0800

    加入音频归一化

--- wav.py
+++ wav.py
@@ -3,6 +3,11 @@ import subprocess
 from pydub import AudioSegment
 import argparse
 import sys
+'''
+截取一段视频
+ffmpeg -ss 00:00:04.473 -t 00:00:01 -i demo.mp4  -c:v libx264 -c:a aac -strict experimental -b:a 98k audio.mp4
+'''
+
 def main(args):
     video=args.video
     wav=args.wav
@@ -10,6 +15,7 @@ def main(args):
     temp_mp3='./temp/temp.mp3'
     temp_video='./temp/output.mp4'
     temp_song='./temp/new_song.mp3'
+    temp_norm='./temp/new_norm.mp3'
     if os.path.exists(temp_mp3):
         os.remove(temp_mp3)
     if os.path.exists(temp_video):
@@ -26,18 +32,22 @@ def main(args):
 
     #分割音频
     song=AudioSegment.from_mp3(temp_mp3)
-    song1=song[:part[0]*1000]
-    song3=song[part[1]*1000:]
+    song1=song[:part[0]]
+    song3=song[part[1] :]
     if 'mp3' in wav:
         song2=AudioSegment.from_mp3(wav)
     elif 'wav' in wav:
         song2=AudioSegment.from_mp3(wav)
     new_song=song1+song2+song3
     new_song.export(temp_song,format='mp3')
+
+    #归一化视频
+    norm="ffmpeg-normalize -f -nt rms -ar 16000 {0} -o {1}".format(temp_song,temp_norm)
+    subprocess.call(norm,shell = True)
     #合成音频视频
     compose='ffmpeg -i %s -i %s %s'%(temp_song,temp_video,'./output/'+video.split('/')[-1])
     subprocess.call(compose,shell = True)
-
+    
 def parse_arguments(argv):
 
     parser = argparse.ArgumentParser()
