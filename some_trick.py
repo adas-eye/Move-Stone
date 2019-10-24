494d6728c19f336d54b62fb75af37f871b1f5bb7
xiang1.zhao <zhaoxiang1@100tal.com>
2019-10-24 18:02:07 +0800

    add average decay

--- some_trick.py
+++ some_trick.py
@@ -12,3 +12,18 @@ tf.summary.scalar('learning_rate', learning_rate)
 update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
 with tf.control_dependencies(update_ops):
     train_op = tf.train.RMSPropOptimizer(learning_rate, decay=config.decay_rate, momentum=config.momentum).minimize(loss, global_step=global_step)
+
+# MOVING_AVERAGE_DECAY
+MOVING_AVERAGE_DECAY = 0.99
+LEARNING_RATE_DECAY = 0.99
+LEARNING_RATE_BASE = 0.8
+variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
+variables_averages_op = variable_averages.apply(tf.trainable_variables())
+learning_rate = tf.train.exponential_decay(
+    LEARNING_RATE_BASE,
+    global_step,
+    mnist.train.num_examples / BATCH_SIZE, LEARNING_RATE_DECAY,
+    staircase=True)
+train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
+with tf.control_dependencies([train_step, variables_averages_op]):
+    train_op = tf.no_op(name='train')
