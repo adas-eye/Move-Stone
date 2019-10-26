074a37c6db189038542ce839672f59df792cf49c
Leslie Zhao <lesliezhao@LesliedeMacBook-Air.local>
2019-10-26 12:30:14 +0800

    scp file

--- some_shell.sh
+++ some_shell.sh
@@ -4,3 +4,6 @@ find . -regex '.*\.txt\|.*\.doc\|.*\.mp3'
 # except some file
 find . -type f ! -name "*.html"   
 
+# skip the file scp
+rsync -avzu --progress -e "ssh -p22" root@xx.xx.xx:/xx/xx/xx ./
+
